256def89ebf6d15767d464a8f5d989fd60ef9d01
Sebastian Nitu <61966540+sebanitu@users.noreply.github.com>
2022-04-18 13:04:05 +0300

    Changed upload URL
    changed Upload url from "App Automate" to "App Live" service

--- step.sh
+++ step.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 set -o pipefail
-curl -u "$browserstack_username:$browserstack_access_key" -X POST https://api-cloud.browserstack.com/app-automate/upload -F "file=@$upload_path" -F 'data={"custom_id": "'$custom_id'"}' | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
+curl -u "$browserstack_username:$browserstack_access_key" -X POST https://api-cloud.browserstack.com/app-live/upload -F "file=@$upload_path" -F 'data={"custom_id": "'$custom_id'"}' | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
